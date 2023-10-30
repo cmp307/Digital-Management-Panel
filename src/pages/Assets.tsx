86d7b25c367dc34e465829318dda05e36153019a
Liam Townsley <contact@liamtownsley.me>
2023-10-30 19:27:37 +0000

    feat: implement useEffect hook for loading data

--- src/pages/Assets.tsx
+++ src/pages/Assets.tsx
@@ -1,46 +1,55 @@
 import Breadcrumbs from "../components/Breadcrumbs";
 import TopBar from "../components/TopBar";
 import '../styles/Assets.scss';
-import {TextBlock, MediaBlock, TextRow, RectShape, RoundShape} from 'react-placeholder/lib/placeholders';
+import { TextBlock, MediaBlock, TextRow, RectShape, RoundShape } from 'react-placeholder/lib/placeholders';
 import ReactPlaceholder from 'react-placeholder';
 import "react-placeholder/lib/reactPlaceholder.css";
 import Table from '../components/Table';
+import { useEffect, useState } from "react";
 
 const tablePlaceholder = (
     <div className='table-placeholder'>
-      <TextBlock rows={3} color='#CDCDCD'/>
+        <TextBlock rows={3} color='#CDCDCD' />
     </div>
-  );
+);
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
 function Assets() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        delay(3000).then(() => {
+            setData([
+                {
+                    id: 'test',
+                    systemName: 'test1',
+                    systemType: 'test2',
+                    managedBy: 'test3'
+                },
+                {
+                    id: 'test1',
+                    systemName: 'test11',
+                    systemType: 'test21',
+                    managedBy: 'test31'
+                }
+            ] as any)
+        });
+    });
+
     return (
         <>
             <TopBar subtext="Asset Management Panel" />
             <Breadcrumbs path="Assets" />
             <h2 className="text-centre">Action Buttons</h2>
             <div id="action-buttons">
-                <button className="btn"><i className="fa fa-plus" /> Create an Asset</button>
-                <button className="btn danger"><i className="fa fa-minus" /> Delete All Assets</button>
+                <button id="create-asset" className="btn"><i className="fa fa-plus" /> Create an Asset</button>
+                <button id="delete-all-assets" className="btn danger"><i className="fa fa-trash" /> Delete <strong>All</strong> Assets</button>
             </div>
             <hr />
             <h2 className="text-centre">Asset List</h2>
-            <p className="text-centre">There is a total of <strong>{0} assets</strong>.</p>
-        <Table isLoading={true} data={[]} />
-            {/* <ReactPlaceholder ready={this.state.ready} customPlaceholder={<tablePlaceholder />}  */}
-            {/* <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">System Name</th>
-                        <th scope="col">System Type</th>
-                        <th scope="col">Managed By</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
+            <p className="text-centre">There is currently <strong>{data.length} {(data.length > 1) ? 'assets' : 'asset'}</strong> stored within the Database.</p>
+            <Table data={data} />
 
-                </tbody>
-            </table> */}
         </>
     )
 }
