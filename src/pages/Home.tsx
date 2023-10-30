814d04ed75b27bd9ef9b7ed022266131ce018dd9
Liam Townsley <contact@liamtownsley.me>
2023-10-30 17:35:13 +0000

    chore: update formatting of index page & add padding to breadcrumbs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,6 +12,7 @@ function Home() {
             <div className="container py-4 px-3 mx-auto">
                 <div className="text-center">
                     <Link to="/assets" className="btn btn-outline-primary" role="button">View &amp; Manage Assets</Link>
+                    <br/><br/>
                     <Link to="/employees" className="btn btn-outline-primary" role="button">View &amp; Manage Employees</Link>
                 </div>
             </div>
