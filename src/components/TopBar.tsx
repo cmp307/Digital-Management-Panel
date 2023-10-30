0fbb5b8bde6de09e7320c7604cfd6b496b317798
Liam Townsley <contact@liamtownsley.me>
2023-10-30 18:18:18 +0000

    feat: update the design of /assets page, and add action buttons

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,11 +1,11 @@
 import '../styles/TopBar.scss'
 
-function TopBar() {
+function TopBar(props: any) {
     return (
         <div id="header">
             <img src="https://i.imgur.com/ZIL6Yos.png" />
             <h1>Scottish Glen</h1>
-            <h3 className="header-2">Asset &amp; Employee Management Panel</h3>
+            <h3 className="header-2">{(props.subtext) ? props.subtext : 'Asset & Employee Management Panel'}</h3>
         </div>
     )
 }
--- src/pages/Assets.tsx
+++ src/pages/Assets.tsx
@@ -1,11 +1,63 @@
 import Breadcrumbs from "../components/Breadcrumbs";
 import TopBar from "../components/TopBar";
+import '../styles/Assets.scss';
 
 function Assets() {
     return (
         <>
-            <TopBar />
+            <TopBar subtext="Asset Management Panel" />
             <Breadcrumbs path="Assets" />
+            <h2 className="text-centre">Action Buttons</h2>
+            <div id="action-buttons">
+                <button className="btn"><i className="fa fa-plus" /> Create an Asset</button>
+                <button className="btn danger"><i className="fa fa-minus" /> Delete All Assets</button>
+            </div>
+            <hr />
+            <h2 className="text-centre">Asset List</h2>
+            <p className="text-centre">There is a total of <strong>{0} assets</strong>.</p>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">System Name</th>
+                        <th scope="col">System Type</th>
+                        <th scope="col">Managed By</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">jbXi3X</th>
+                        <td>Front Door</td>
+                        <td>Door Monitor</td>
+                        <td><a href="#">Liam T.</a></td>
+                        <td>
+                            <button type="button" className="btn btn-primary">Edit Asset</button>
+                            <button type="button" className="btn btn-danger">Delete Asset</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">jbXi3X</th>
+                        <td>Front Door</td>
+                        <td>Door Monitor</td>
+                        <td><a href="#">Liam T.</a></td>
+                        <td>
+                            <button type="button" className="btn btn-primary">Edit Asset</button>
+                            <button type="button" className="btn btn-danger">Delete Asset</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">jbXi3X</th>
+                        <td>Front Door</td>
+                        <td>Door Monitor</td>
+                        <td><a href="#">Liam T.</a></td>
+                        <td>
+                            <button type="button" className="btn btn-primary">Edit Asset</button>
+                            <button type="button" className="btn btn-danger">Delete Asset</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </>
     )
 }
