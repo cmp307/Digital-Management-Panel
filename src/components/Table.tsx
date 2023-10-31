e7414fc8666e94d0f19a60e8dadf284407f47e35
Liam Townsley <contact@liamtownsley.me>
2023-10-31 01:46:25 +0000

    feat: adapt Table.tsx code to match new database schema

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,8 @@
 import { TextBlock } from "react-placeholder/lib/placeholders";
 import PillButton from "./PillButton";
+import { Link } from "react-router-dom";
 
-const YourComponent = ({ data }) => {
+const YourComponent = ({ data }: any) => {
     if(data.length == 0) {
         return <TextBlock rows={3} color='#CDCDCD' />
     }
@@ -20,11 +21,11 @@ const YourComponent = ({ data }) => {
                 </thead>
                 <tbody>
                     {data.map((item:any) => (
-                        <tr key={item.id}>
-                            <td><code>{item.id}</code></td>
-                            <td>{item.systemName}</td>
-                            <td><PillButton data={item.systemType} /></td>
-                            <td>{item.managedBy}</td>
+                        <tr key={item._id}>
+                            <td><code>{item._id}</code></td>
+                            <td>{item.name}</td>
+                            <td><PillButton data={item.type} /></td>
+                            <td><Link to={`/employee/${item.managedBy.uid}`}>{item.managedBy.name}</Link></td>
                             <td>
                                 <button type="button" className="btn btn-primary">Edit Asset</button>
                                 <button type="button" className="btn btn-danger">Delete Asset</button>
