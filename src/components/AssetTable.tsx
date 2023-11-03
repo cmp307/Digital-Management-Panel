5c95f592d202af03d40acbb3d69edb30d4bd1819
Liam Townsley <contact@liamtownsley.me>
2023-11-03 22:19:27 +0000

    feat: further implement the assets pages and add subpage with more detailed information

--- src/components/AssetTable.tsx
+++ src/components/AssetTable.tsx
@@ -15,7 +15,7 @@ const YourComponent = ({ data }: any) => {
                         <th scope="col">Asset ID</th>
                         <th scope="col">System Name</th>
                         <th scope="col">System Type</th>
-                        <th scope="col">Managed By</th>
+                        {/* <th scope="col">Managed By</th> */}
                         <th scope="col"></th>
                     </tr>
                 </thead>
@@ -25,7 +25,7 @@ const YourComponent = ({ data }: any) => {
                             <td><code>{item._id}</code></td>
                             <td>{item.name}</td>
                             <td><PillButton data={item.type} /></td>
-                            <td><Link to={`/employee/${item.managedBy.uid}`}>{item.managedBy.name}</Link></td>
+                            {/* <td><Link to={`/employee/${item.employee}`}>{item.employee}</Link></td> */}
                             <td>
                                 <Link to={`/assets/${item._id}`} role="button" id="blue-button" className="btn btn-outline-primary"><i className="fa fa-eye" /> View Asset</Link>
                                 <Link to={`/edit/assets/${item._id}`} role="button" id="blue-button" className="btn btn-outline-primary"><i className="fa fa-edit" /> Edit Asset</Link>
@@ -33,9 +33,7 @@ const YourComponent = ({ data }: any) => {
                                     fetch(`http://127.0.0.1:3001/api/assets/${item._id}/delete`, { method: 'DELETE' }).then(() => {
                                         refreshPage();
                                     })
-                                }} className="btn btn-outline-danger"><i className="fa fa-trash" /> Delete Asset1</button>
-                                {/* <Link to={`/delete/assets/${item._id}`} role="button" id="blue-button" className="btn btn-outline-danger"><i className="fa fa-trash" /> Delete Asset</Link> */}
-
+                                }} className="btn btn-outline-danger"><i className="fa fa-trash" /> Delete Asset</button>
                             </td>
                         </tr>
                     ))}
@@ -45,6 +43,12 @@ const YourComponent = ({ data }: any) => {
         </div>
     );
 };
+
+// function deleteAsset(id: string) {
+//     fetch(`http://127.0.0.1:3001/delete/assets/${id}`, { method: 'DELETE' }).then(() => {
+//         refreshPage();
+//     })
+// }
 
 function refreshPage() {
     window.location.reload();
--- src/pages/CreateAsset.tsx
+++ src/pages/CreateAsset.tsx
@@ -1,29 +1,20 @@
-import { useState } from "react";
 import Breadcrumbs from "../components/Breadcrumbs";
 import TopBar from "../components/TopBar";
 import '../styles/AssetsCreate.scss';
 import "react-placeholder/lib/reactPlaceholder.css";
+import { useEffect, useState } from "react";
 
 function CreateAssets() {
-    const [name, setName] = useState(undefined as any);
-    const [type, setType] = useState(undefined as any);
-    const [managerName, setManagerName] = useState(undefined as any);
-    const [managerID, setManagerID] = useState(0 as any);
+    const [data, setData] = useState([] as any[]);
 
-    function sendAssetForm() {
-        fetch('http://127.0.0.1:3001/api/assets/create', {
-            method: 'POST',
-            body: JSON.stringify({
-                name,
-                type,
-                managerBy: {
-                    name: managerName,
-                    uid: managerID
-                }
-            })
-        })
-    }
+    useEffect(() => {
+        fetch('http://127.0.0.1:3001/api/employees')
+            .then((res) => res.json())
+            .then((res) => setData(res))
+            .catch((err) => console.error(err))
+    }, []);
 
+    console.log(data);
 
     return (
         <>
@@ -35,23 +26,58 @@ function CreateAssets() {
             ]} />
             <h2 className="text-centre">Create an Asset</h2>
 
-            <form id="create-asset" onSubmit={sendAssetForm}>
+            <form id="create-asset" method="get" action="http://localhost:3001/api/assets/create">
                 <div id="question">
                     <label htmlFor="name">Asset Name</label><br />
-                    <input type="text" id="name" placeholder="Please enter the name for your Asset." onChange={(e) => setName(e.target.value)}></input>
+                    <input type="text" id="name" name="name" placeholder="Please enter the name for your Asset."></input>
                 </div>
                 <div id="question">
                     <label htmlFor="type">Asset Type</label><br />
-                    <input type="text" id="type" placeholder="Please enter the type of the Asset." onChange={(e) => setType(e.target.value)}></input>
+                    <input type="text" id="type" name="type" placeholder="Please enter the type of the Asset."></input>
                 </div>
+
                 <div id="question">
+                    <label htmlFor="type">Asset Model</label><br />
+                    <input type="text" id="type" name="model" placeholder="Please enter the Model of the Asset."></input>
+                </div>
+
+                <div id="question">
+                    <label htmlFor="type">Asset Manufacturer</label><br />
+                    <input type="text" id="type" name="manufacturer" placeholder="Please enter the Manufacturer of the Asset."></input>
+                </div>
+                
+
+                <div id="question">
+                    <label htmlFor="type">IP Address</label><br />
+                    <input type="text" id="type" name="ip" placeholder="Please enter the IP Address of the Asset."></input>
+                </div>
+                
+                <div id="question">
+                    <label htmlFor="type">Purchase Date</label><br />
+                    <input type="text" id="type" name="date" placeholder="Please enter the Purchase Date of the Asset."></input>
+                </div>
+
+                <div id="question">
+                    <label htmlFor="type">Note</label><br />
+                    <input type="text" id="type" name="note" placeholder="Would you like to leave a note on this asset?"></input>
+                </div>
+
+                <label>What employee do you want to assign this asset to?</label>
+                <select name="employee" id="employee">
+                    <option value="1">(1) John S.</option>
+                    <option value="2">(2) Steve E.</option>
+                    <option value="3">(3) Mick B.</option>
+                    <option value="4">(4) Leon F.</option>
+                </select>
+                
+                {/* <div id="question">
                     <label htmlFor="type">Manager Name</label><br />
-                    <input type="text" id="type" placeholder="Please enter the name of the Asset Manager." onChange={(e) => setManagerName(e.target.value)}></input>
+                    <input type="text" id="type" name="manager" placeholder="Please enter the name of the Asset Manager."></input>
                 </div>
                 <div id="question">
                     <label htmlFor="type">Manager ID</label><br />
-                    <input type="text" id="type" placeholder="Please enter the ID of the Asset Manager." onChange={(e) => setManagerID(e.target.value)}></input>
-                </div>
+                    <input type="text" id="type" name="uid" placeholder="Please enter the ID of the Asset Manager."></input>
+                </div> */}
 
                 <br />
 
