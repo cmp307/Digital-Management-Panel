4eb85e8e429d5a8c9a8425dacca06106072f6580
Liam Townsley <contact@liamtownsley.me>
2023-11-03 22:20:17 +0000

    feat: basic implementation of employee page & update topbar subtext

--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
 import Breadcrumbs from "../components/Breadcrumbs";
 import TopBar from "../components/TopBar";
 
 function Employees() {
+    const [data, setData] = useState([] as any[]);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:3001/api/employees')
+            .then((res) => res.json())
+            .then((res) => setData(res))
+            .catch((err) => console.error(err))
+    }, []);
+
+    console.log(data);
+
     return (
         <>
-            <TopBar />
+            <TopBar subtext="Employee Management Panel" />
             <Breadcrumbs data={[
                 { name: 'Home', path: '/' },
                 { name: 'Employees', path: '/employees' },
-            ]} />        </>
+            ]} />
+        </>
     )
 }
 
