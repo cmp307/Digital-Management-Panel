487cddc0da27b268ac02f6228d1cedb3c506d073
Liam Townsley <contact@liamtownsley.me>
2023-11-03 21:26:08 +0000

    feat: add eror 404

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import Assets from './pages/Assets'
 import './styles/index.scss'
 import Employees from './pages/Employees'
 import CreateAssets from './pages/CreateAsset'
+import Error404 from './pages/Error404'
+import Asset from './pages/Asset'
+import Employee from './pages/Employee'
 
 function App() {
-  
+
   return (
     <>
       <HashRouter>
         <Routes>
-          <Route index element={<Home />}></Route>
-          <Route path="/assets" element={<Assets />}></Route>
-          <Route path="/assets/create" element={<CreateAssets />}></Route>
-          <Route path="/employees" element={<Employees />}></Route>
+          <Route index element={<Home />} />
+          <Route path="/assets" element={<Assets />} />
+          <Route path="/assets/:id" element={<Asset />} />
+          <Route path="/assets/create" element={<CreateAssets />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/employees/:id" element={<Employee />} />
+          <Route path='*' element={<Error404 />} />
         </Routes>
       </HashRouter>
     </>
