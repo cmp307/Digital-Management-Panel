322b1e27cab5adf05eba00112d95f621bdeda2c8
Liam Townsley <contact@liamtownsley.me>
2023-11-03 22:18:17 +0000

    feat: make the topbar link back to the index page

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import '../styles/TopBar.scss'
 
 function TopBar(props: any) {
     return (
         <div id="header">
-            <img src="https://i.imgur.com/ZIL6Yos.png" />
-            <h1>Scottish Glen</h1>
-            <h3 className="header-2">{(props.subtext) ? props.subtext : 'Asset & Employee Management Panel'}</h3>
+            <Link to={'/'}>
+                <img src="https://i.imgur.com/ZIL6Yos.png" />
+                <h1>Scottish Glen</h1>
+                <h3 className="header-2">{(props.subtext) ? props.subtext : 'Asset & Employee Management Panel'}</h3>
+            </Link>
         </div>
     )
 }
