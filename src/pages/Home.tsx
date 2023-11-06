e353d73c468b10b636658da4f308a83a246a84a9
Liam Townsley <contact@liamtownsley.me>
2023-11-06 16:19:23 +0000

    fix: topbar no longer hardcoded within the Home.tsx file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,10 @@
 import { Link } from "react-router-dom";
+import TopBar from "../components/TopBar";
 
 function Home() {
     return (
         <>
-            <div id="header">
-                <img src="https://i.imgur.com/ZIL6Yos.png" className="img-fluid mx-auto d-block" />
-                <h1>Scottish Glen</h1>
-                <h3 className="header-2">Asset &amp; Employee Management Panel</h3>
-            </div>
+            <TopBar />
             <div className="container py-4 px-3 mx-auto">
                 <div className="text-center">
                     <Link to="/assets" className="btn btn-outline-primary" role="button">View &amp; Manage Assets</Link>
