7d36d8008898f833253e5939bf7b9671631a2d9b
Liam Townsley <contact@liamtownsley.me>
2023-11-06 15:45:43 +0000

    refactor: refacfor AssetInfoTable page to use OOP.

--- src/components/AssetInfoTable.tsx
+++ src/components/AssetInfoTable.tsx
@@ -1,50 +1,53 @@
-import { TextBlock } from "react-placeholder/lib/placeholders"
+import { Component } from "react";
+import { Asset } from "../interfaces/Asset";
 
-const AssetInfoTable = ({ data }: any) => {
-    if (!data) {
-        return <TextBlock rows={3} color='#CDCDCD' />
+class AssetInfoTable extends Component<{ asset: Asset }> {
+    constructor(props: any) {
+        super(props);
     }
 
-    return (
-        <table id="single-asset-table" className="table">
-            <tr>
-                <th><i className="fa fa-id-card-o" />Unique Asset ID</th>
-                <td>{data._id}</td>
-            </tr>
-            <tr>
-                <th><i className="fa fa-pencil-square-o" /> Name</th>
-                <td>{data.name ?? '-'}</td>
-            </tr>
-            <tr>
-                <th><i className="fa fa-list-alt" /> Model</th>
-                <td>{data.model ?? '-'}</td>
-            </tr>
-            <tr>
-                <th><i className="fa fa-cogs" /> Manufacturer</th>
-                <td>{data.manufacturer ?? '-'}</td>
-            </tr>
-            <tr>
-                <th><i className="fa fa-exchange" /> Asset Type</th>
-                <td>{data.type ?? '-'}</td>
-            </tr>
-            <tr>
-                <th><i className="fa fa-wifi" /> IP Address</th>
-                <td>{data.ip ?? '-'}</td>
-            </tr>
-            <tr>
-                <th><i className="fa fa-user" /> Supervising Employee</th>
-                <td><a href="#">Liam T.</a><code>(ID: {data.employee})</code></td>
-            </tr>
-            <tr>
-                <th><i className="fa fa-calendar" /> Purchase Date</th>
-                <td>{data.date} <code>(15 days ago)</code></td>
-            </tr>
-            <tr>
-                <th><i className="fa fa-sticky-note-o" /> Note</th>
-                <td>{data.note}</td>
-            </tr>
-        </table>
-    )
+    render() {
+        return (
+            <table id="single-asset-table" className="table">
+                <tr>
+                    <th><i className="fa fa-id-card-o" />Unique Asset ID</th>
+                    <td>{this.props.asset._id.toString() ?? '-'}</td>
+                </tr>
+                <tr>
+                    <th><i className="fa fa-pencil-square-o" /> Name</th>
+                    <td>{this.props.asset.name ?? '-'}</td>
+                </tr>
+                <tr>
+                    <th><i className="fa fa-list-alt" /> Model</th>
+                    <td>{this.props.asset.model ?? '-'}</td>
+                </tr>
+                <tr>
+                    <th><i className="fa fa-cogs" /> Manufacturer</th>
+                    <td>{this.props.asset.manufacturer ?? '-'}</td>
+                </tr>
+                <tr>
+                    <th><i className="fa fa-exchange" /> Asset Type</th>
+                    <td>{this.props.asset.type ?? '-'}</td>
+                </tr>
+                <tr>
+                    <th><i className="fa fa-wifi" /> IP Address</th>
+                    <td>{this.props.asset.ip ?? '-'}</td>
+                </tr>
+                <tr>
+                    <th><i className="fa fa-user" /> Supervising Employee</th>
+                    <td><a href="#">Liam T.</a><code>(ID: {this.props.asset.employee})</code></td>
+                </tr>
+                <tr>
+                    <th><i className="fa fa-calendar" /> Purchase Date</th>
+                    <td>{this.props.asset.date} <code>(15 days ago)</code></td>
+                </tr>
+                <tr>
+                    <th><i className="fa fa-sticky-note-o" /> Note</th>
+                    <td>{this.props.asset.note}</td>
+                </tr>
+            </table>
+        )
+    }
 }
 
 export default AssetInfoTable;
