d9f5531ec501df2c2615331a51735a8295e07367
Liam Townsley <contact@liamtownsley.me>
2023-11-15 14:58:39 +0000

    chore: visual fix for breadcrumbs on index page

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -4,7 +4,7 @@ import { Component } from 'react';
 import { BreadcrumbHistory } from "../interfaces/Breadcrumbs";
 import { Employee } from "../interfaces/Employee";
 
-class Breadcrumbs extends Component<{ username: string, history?: BreadcrumbHistory[], setUser:Function }> {
+class Breadcrumbs extends Component<{ username: string, history?: BreadcrumbHistory[], setUser: Function }> {
     constructor(props: any) {
         super(props);
         this.state = {
@@ -40,7 +40,9 @@ class Breadcrumbs extends Component<{ username: string, history?: BreadcrumbHist
         };
         return (
             <div id="breadcrumbs">
-                <p className="left-aligned"></p>
+                <p className="left-aligned">
+                    <p className="left-aligned"><i className="fa fa-history" /> <strong>Breadcrumbs »</strong> Home</p>
+                </p>
                 <p className="right-aligned">
                     <p><i className="fa fa-user" /> {this.props.username}</p>
                     <button className="btn btn-danger" onClick={this.logout}><i className="fa fa-sign-out" /> Logout</button>
