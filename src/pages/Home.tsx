bf43c1647013a4202d7ecbe0d69579317f08dc1e
Liam Townsley <contact@liamtownsley.me>
2023-11-19 13:09:57 +0000

    fix: inaccurate link to /hardware on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,7 +25,7 @@ class Home extends Component<{ setUser: Function, user: IEmployee }, { user: IEm
                         <br />
                         <h1>Welcome to the ScottishGlen Administration Panel!</h1>
                         <br />
-                        <p>From here you can manage the <Link to={'/assets'}>Hardware</Link> & <Link to={'/software'}>Software</Link> assets and use it to get a general overview of the assets contained within the company.</p>
+                        <p>From here you can manage the <Link to={'/hardware'}>Hardware</Link> & <Link to={'/software'}>Software</Link> assets and use it to get a general overview of the assets contained within the company.</p>
                         <p>It can also be used for Vulnerability Scanning through the <a href='https://nvd.nist.gov/' target='_blank'>National Vulnerability Database</a>. If at anytime you would like to return to this page, click the logo at the top of your screen!</p>
                     </div>
                 </div>
@@ -34,7 +34,7 @@ class Home extends Component<{ setUser: Function, user: IEmployee }, { user: IEm
                         <Link to="/hardware" className="btn btn-outline-primary" role="button"><i className="fa fa-server" /> View &amp; Manage Hardware Assets</Link>
                         <Link to="/software" className="btn btn-outline-primary" role="button"><i className="fa fa-cloud-download" /> View &amp; Manage Software Assets</Link>
                         <Link to="/employees" className="btn btn-outline-primary" role="button"><i className="fa fa-sitemap" /> View &amp; Manage Employees</Link>
-                        <Link to="/versions" className="btn btn-outline-secondary" role="button"><i className="fa fa-info" /> View Software Information</Link>
+                        <Link to="/versions" className="btn btn-outline-secondary" role="button"><i className="fa fa-info-circle" /> View Software Information</Link>
                     </div>
                 </div>
             </>
