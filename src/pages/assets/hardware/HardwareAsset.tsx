bde50a882d17588baf8c1fd869385d93e3373121
Liam Townsley <contact@liamtownsley.me>
2023-11-19 13:09:36 +0000

    feat: further implement hardware/software link into software assets

--- src/pages/assets/hardware/HardwareAsset.tsx
+++ src/pages/assets/hardware/HardwareAsset.tsx
@@ -10,11 +10,10 @@ import { HardwareAsset as CHardwareAsset } from "../../../components/assets/hard
 import { IEmployee } from "../../../interfaces/Employee";
 import Breadcrumbs from "../../../components/Breadcrumbs";
 import HardwareAssetInfoTable from "../../../components/assets/hardware/HardwareAssetInfoTable";
-import SoftwareAssetTable from "../../../components/assets/software/SoftwareAssetTable";
 import { SoftwareAsset } from "../../../components/assets/software/SoftwareAsset";
 import SoftwareLinkTable from "../../../components/assets/software/SoftwareLinkTable";
 
-class HardwareAsset extends Component<{ setUser: Function, user: IEmployee, id: string, navigate: NavigateFunction }, { hardware_data?: CHardwareAsset, software_data?: SoftwareAsset[], setUser: Function, user: IEmployee }> {
+class HardwareAsset extends Component<{ setUser: Function, user: IEmployee, id: string, navigate: NavigateFunction }, { hardware_data?: CHardwareAsset, software_data?: { software: SoftwareAsset, link: any }[], setUser: Function, user: IEmployee }> {
     private _id: string;
     constructor(props: any) {
         super(props)
@@ -34,7 +33,7 @@ class HardwareAsset extends Component<{ setUser: Function, user: IEmployee, id: 
             .then((res) => this.setState({ hardware_data: res }))
             .catch((err) => console.error(err))
 
-        fetch(`http://127.0.0.1:3001/api/asset-link/hardware/${this._id}`)
+        fetch(`http://127.0.0.1:3001/api/asset-link/hardware/${this._id}/get-all`)
             .then((res) => res.json())
             .then((res) => res.map((x: any) => { return { software: new SoftwareAsset(x.software), link: x.link } }))
             .then((res) => this.setState({ software_data: [...res] }))
@@ -61,17 +60,22 @@ class HardwareAsset extends Component<{ setUser: Function, user: IEmployee, id: 
                     { name: 'Hardware Assets', path: '/hardware' },
                     { name: this._id ?? '-', path: `/hardware/${this._id}` },
                 ]} setUser={this.props.setUser} username={this.props.user.email} />
-
-                <h2 className="text-centre">Action Buttons</h2>
+                <div className="hero">
+                    <div id="spacer"></div>
+                    <h2 className="text-centre"><i className="fa fa-terminal" /> Action Buttons</h2>
+                    <hr />
+                </div>
                 <div id="action-buttons">
                     <Link to={`/hardware/${this._id}/install`} className="btn btn-outline-primary"><i className="fa fa-tasks" /> Link Software</Link>
                     <Link to={`/hardware/${this._id}/edit`} className="btn btn-outline-primary"><i className="fa fa-edit" /> Edit Asset</Link>
                     <button onClick={this.refreshPage} className="btn btn-outline-primary"><i className="fa fa-refresh" /> Refresh Asset</button>
                     <button onClick={this.delete} className="btn btn-outline-danger"><i className="fa fa-trash" /> Delete Asset</button>
                 </div>
-                <hr />
-                <h2 className="text-centre">Hardware Asset Information</h2>
-                <hr />
+                <div className="hero">
+                    <hr />
+                    <h2 className="text-centre"><i className="fa fa-server" /> Hardware Asset Information</h2>
+                    <hr />
+                </div>
                 {this.state.hardware_data ?
                     <div id="centred-div">
                         <HardwareAssetInfoTable asset={this.state.hardware_data} />
@@ -81,9 +85,11 @@ class HardwareAsset extends Component<{ setUser: Function, user: IEmployee, id: 
 
                 {this.state.software_data && this.state.software_data.length > 0 ?
                     <div className="text-centre">
-                        <hr />
-                        <h2 className="text-centre">Linked Software Assets</h2>
-                        <hr />
+                        <div className="hero">
+                            <hr />
+                            <h2 className="text-centre"><i className="fa fa-cloud-download" /> Linked Software Assets</h2>
+                            <hr />
+                        </div>
                         <div>
                             <SoftwareLinkTable assets={this.state.software_data} id={this.props.id} />
                             <button className="btn btn-outline-primary" onClick={() => this.props.navigate(-1)}>Return to previous page!</button>
--- src/pages/assets/software/SoftwareAsset.tsx
+++ src/pages/assets/software/SoftwareAsset.tsx
@@ -10,25 +10,37 @@ import { SoftwareAsset as CSoftwareAsset } from "../../../components/assets/soft
 import { IEmployee } from "../../../interfaces/Employee";
 import Breadcrumbs from "../../../components/Breadcrumbs";
 import SoftwareAssetInfoTable from "../../../components/assets/software/SoftwareAssetInfoTable";
+import HardwareLinkTable from "../../../components/assets/hardware/HardwareLinkTable";
+import { HardwareAsset } from "../../../components/assets/hardware/HardwareAsset";
 
-class SoftwareAsset extends Component<{ setUser: Function, user: IEmployee, id: string, navigate: NavigateFunction }, { setUser: Function, user: IEmployee, data?: CSoftwareAsset }> {
+class SoftwareAsset extends Component<{ setUser: Function, user: IEmployee, id: string, navigate: NavigateFunction }, { setUser: Function, user: IEmployee, software_data?: CSoftwareAsset, hardware_data?: { hardware: HardwareAsset, link: any }[] }> {
     private _id: string;
     constructor(props: any) {
         super(props)
         this.state = {
+            hardware_data: undefined,
             setUser: props.setUser,
             user: props.user,
-            data: undefined
+            software_data: undefined
         }
         this._id = props.id
     }
 
     componentDidMount() {
+        console.log(`fetching /assets/software/${this._id}`);
         fetch(`http://127.0.0.1:3001/api/assets/software/${this._id}`)
             .then((res) => res.json())
             .then((res) => new CSoftwareAsset(res))
-            .then((res) => this.setState({ data: res }))
+            .then((res) => {
+                this.setState({ software_data: res })
+            })
             .catch((err) => console.error(err))
+
+        console.log(`fetching /asset-link/software/${this._id}/get-all`);
+        fetch(`http://127.0.0.1:3001/api/asset-link/software/${this._id}/get-all`)
+            .then((res) => res.json())
+            .then((res) => this.setState({ hardware_data: res }))
+            .then((res) => console.log('fetched', res))
     }
 
     delete() {
@@ -42,6 +54,8 @@ class SoftwareAsset extends Component<{ setUser: Function, user: IEmployee, id: 
     }
 
     render() {
+        console.log(this._id);
+        console.log(this.state);
         return (
             <>
                 <TopBar />
@@ -50,23 +64,42 @@ class SoftwareAsset extends Component<{ setUser: Function, user: IEmployee, id: 
                     { name: 'Software Assets', path: '/software' },
                     { name: this._id ?? '-', path: `/software/${this._id}` },
                 ]} setUser={this.props.setUser} username={this.props.user.email} />
-
-                <h2 className="text-centre">Action Buttons</h2>
+                <div className="hero">
+                    <div id="spacer"></div>
+                    <h2 className="text-centre"><i className="fa fa-terminal" /> Action Buttons</h2>
+                    <hr />
+                </div>
                 <div id="action-buttons">
                     <Link to={`/software/${this._id}/scan`} role="button" id="blue-button" className="btn btn-outline-secondary disabled"><i className="fa fa-dot-circle-o" /> Scan Asset</Link>
                     <Link to={`/software/${this._id}/edit`} className="btn btn-outline-primary"><i className="fa fa-edit" /> Edit Asset</Link>
                     <button onClick={this.refreshPage} className="btn btn-outline-primary"><i className="fa fa-refresh" /> Refresh Asset</button>
                     <button onClick={this.delete} className="btn btn-outline-danger"><i className="fa fa-trash" /> Delete Asset</button>
                 </div>
-                <hr />
-                <h2 className="text-centre">Software Asset Information</h2>
-                <hr />
+                <div className="hero">
+                    <hr />
+                    <h2 className="text-centre"><i className="fa fa-cloud-download" /> Software Asset Information</h2>
+                    <hr />
+                </div>
                 <div id="centred-div">
-                    {this.state.data ? <SoftwareAssetInfoTable asset={this.state.data} /> : 'Loading...'}
-                    <button className="btn btn-outline-primary" onClick={() => this.props.navigate(-1)}>Go back to previous page!</button>
+                    {this.state.software_data ? <SoftwareAssetInfoTable asset={this.state.software_data} /> : 'Loading...'}
                     <br /><br />
                 </div>
 
+                {this.state.hardware_data && this.state.hardware_data.length > 0 ?
+                    <div className="text-centre">
+                        <div className="hero">
+                            <hr />
+                            <h2 className="text-centre"><i className="fa fa-server" /> Linked Hardware Assets</h2>
+                            <hr />
+                        </div>
+                        <div>
+                            <HardwareLinkTable assets={this.state.hardware_data} id={this.props.id} />
+                        </div>
+                    </div> : <></>}
+                <div id="centred-div">
+                    <button className="btn btn-outline-primary" onClick={() => this.props.navigate(-1)}>Return to previous page!</button>
+                    <br />
+                </div>
             </>
         )
     }
