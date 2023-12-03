2a2c5e2e057e40edb5e7026c7e14b28d9d85f9de
Liam Townsley <contact@liamtownsley.me>
2023-12-03 16:51:42 +0000

    feat: update create hardware asset test

--- tests/main-app/3-create-hardware-asset.test.mjs
+++ tests/main-app/3-create-hardware-asset.test.mjs
@@ -7,7 +7,9 @@ const delay = ms =>
             resolve();
         }, ms);
     });
-    
+
+export let INSERTED_ASSET_ID = undefined;
+
 export default async (page) => new Promise(async (resolve, reject) => {
     try {
         const view_hardware = await page.waitForSelector('a::-p-text(View & Manage Hardware Assets)');
@@ -17,34 +19,45 @@ export default async (page) => new Promise(async (resolve, reject) => {
         await create_asset.click();
 
         const name = await page.waitForSelector('input#name');
+        await name.click();
         await name.type('Windows 10');
 
         const type = await page.waitForSelector('select#type');
         await type.select("Workstation");
-        
+
         const model = await page.waitForSelector('input#model');
+        await model.click();
         await model.type("B460HD3");
 
         const manufacturer = await page.waitForSelector('input#manufacturer')
+        await manufacturer.click();
         await manufacturer.type('Gigabyte Technology Co., Ltd.')
-        
+
         const ip = await page.waitForSelector('input#ip')
-        await ip.type(`192.168.0.${Math.floor(Math.random() * 255) + 1}`);
-        
+        await ip.click();
+        await ip.type(`192.168.0.255`);
+
         const date = await page.waitForSelector('input#date')
         await date.type(new Date().toLocaleDateString('en-GB').split('/').join('-'))
-        
+
         const note = await page.waitForSelector('textarea#note');
+        await note.click();
         await note.type("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices metus porttitor ullamcorper tempor. Vivamus euismod elit in magna rutrum, eget congue sem sodales. Aenean porta feugiat tempor. Fusce vehicula luctus enim, nec tristique sapien aliquet ac. Proin in vulputate velit.");
-        
+
         const employee = await page.waitForSelector('select#parent_employee');
         await employee.select("Demo User (Information Technology)");
 
         const submit = await page.waitForSelector('input[type="submit"]');
         await submit.click();
 
-        await delay(5000);
-
+        const response = await page.waitForResponse(
+            response =>
+                response.url().endsWith("/assets/hardware") && response.status() === 200
+        );
+        
+        const res_json = await response.json();
+        if(res_json.status == true) { INSERTED_ASSET_ID = res_json.id }
+        else { reject("Asset not inserted into database!") } 
         resolve(true);
     } catch (error) {
         reject(error)
